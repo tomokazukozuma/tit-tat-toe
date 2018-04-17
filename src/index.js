ee42b955024f74aec35a852135f305bca077ef1c
tomokazu kozuma <tomokazu.kozuma@gmail.com>
2018-04-17 09:50:52 +0900

    Delete Link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import './index.css';
 
